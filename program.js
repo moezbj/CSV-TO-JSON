22fe9c51030520f109a79f7f6490b2cebb5217ba
moezbj <moezbj.93@gmail.com>
2017-12-06 14:55:40 +0100

    version 1

--- program.js
+++ program.js
@@ -1,7 +1,9 @@
 const fs= require ('fs')
 const csv =require('fast-csv')
 
-fs.createReadStream('customer-data.csv')
+
+var res=fs.createReadStream('customer-data.csv')
+// const solution=fs.createReadStream('Data.json')
   .pipe(csv())
   .on('data',function (data){
   console.log(data)
